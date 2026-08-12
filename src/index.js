3bb2fc5a1cceae82f41b1942c66e347e3d0337ef
Haifaa Alkaabi <hma.alkaabi99@gmail.com>
2026-08-12 07:34:26 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { onRequest as yaziHandler } from "../functions/api/yazi.js";
+import { onRequest as modelApplicationsHandler } from "../functions/api/model-applications.js";
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
     if (url.pathname === "/api/yazi") {
-      return yaziHandler({ request, env });
+      return yaziHandler({ request, env, ctx });
+    }
+
+    if (url.pathname === "/api/model-applications") {
+      return modelApplicationsHandler({ request, env, ctx });
     }
 
     return env.ASSETS.fetch(request);
